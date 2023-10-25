9d36c3e5e8c0b2b1ca65fbec7064dd2a379709fb
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-10-25 13:07:16 +0200

    Add a function to determine the adjacency list of each coordinate

--- script.js
+++ script.js
@@ -13,6 +13,30 @@ function checkValidity(current, target) {
   console.log('Please pass valid coordinates to the knightMoves function.');
 }
 
+function getAdjacentCoordinates(current) {
+  let [x, y] = current.coordinate;
+  let adjacencyList = [
+    [x - 1, y - 2],
+    [x - 2, y - 1],
+    [x - 2, y + 1],
+    [x - 1, y + 2],
+    [x + 1, y + 2],
+    [x + 2, y + 1],
+    [x + 2, y - 1],
+    [x + 1, y - 2],
+  ];
+  adjacencyList = adjacencyList.filter((coordinate) => {
+    const [x, y] = coordinate;
+    if (x >= 0 && y >= 0 && x <= 7 && y <= 7) {
+      return true;
+    }
+  });
+  adjacencyList = adjacencyList.map((coordinate) => {
+    return { coordinate, previousCoordinate: current };
+  });
+  return adjacencyList;
+}
+
 function printBoard(path) {
   const board = [];
   for (let i = 0; i < 8; i += 1) {
