2fd5884e06cb5f7b770e3b5e8faecd8ed50d35d8
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-10-25 13:05:52 +0200

    Add a function to determine the validity of an input

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function checkValidity(current, target) {
+  if (Array.isArray(current) && Array.isArray(target)) {
+    if (current.length === 2 && target.length === 2) {
+      const [x, y] = current;
+      const [j, k] = target;
+      if (x >= 0 && y >= 0 && x <= 7 && y <= 7) {
+        if (j >= 0 && k >= 0 && j <= 7 && k <= 7) {
+          return true;
+        }
+      }
+    }
+  }
+  console.log('Please pass valid coordinates to the knightMoves function.');
+}
+
 function printBoard(path) {
   const board = [];
   for (let i = 0; i < 8; i += 1) {
